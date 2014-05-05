ae58dc70b514be50a586b6551ce47a445345a049
Sterling Radcliff <radcliff@riseup.net>
2014-05-05 01:41:17 -0700

    hid secrets in ENV, forcing ssl

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-GardenShare::Application.config.secret_key_base = 'a9beb3ed49dccd1a16ad0a68e8106ceeae8b72473fd12e5b2b4c8f41e85126c4a89063c362911cc4ec58cc2ab0f8330dc23c64696142395556f21774275ed5c9'
+GardenShare::Application.config.secret_token = ENV['SECRET_KEY_BASE']
