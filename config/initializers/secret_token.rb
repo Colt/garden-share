d2a6f35b9d11d4a3e321bbd92d5c53dbb85ae61f
Sterling Radcliff <radcliff@riseup.net>
2014-05-05 00:32:56 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GardenShare::Application.config.secret_key_base = 'a9beb3ed49dccd1a16ad0a68e8106ceeae8b72473fd12e5b2b4c8f41e85126c4a89063c362911cc4ec58cc2ab0f8330dc23c64696142395556f21774275ed5c9'
