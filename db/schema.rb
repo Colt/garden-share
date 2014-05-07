0deaa9d5793df9eacee374a97d4eebf1c9c214c9
Sterling Radcliff <radcliff@riseup.net>
2014-05-06 18:50:50 -0700

    rails destroy model User & rails generate migration DropUsersTable & db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140505194359) do
+ActiveRecord::Schema.define(version: 20140507014632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "gardens", force: true do |t|
     t.string   "location"
-    t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
