3d7278c6b85b3d60b00453458bc634ab7a2d94fd
Sterling Radcliff <radcliff@riseup.net>
2014-05-09 10:25:34 -0700

    added validations to Garden model

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,6 +1,12 @@
 class Garden < ActiveRecord::Base
   has_many :users
 
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :short_description, presence: true
+  validates :description, presence: true
+  validates :rules, presence: true
+
   has_attached_file :photo, :styles => { :medium => "300x300#", :thumb => "200x200#" }, :default_url => "/images/:style/succulent.png"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
